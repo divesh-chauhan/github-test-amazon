d64a33cd311c558d34f44a775477ef429d7b4f0b
divesh chauhan <diveshc207@gmail.com>
2025-03-28 11:16:55 +0530

    remove error

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(session({
         maxAge : 5 * 60 * 1000
     }
 }));
-clea
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
