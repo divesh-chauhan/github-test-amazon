6db7a98684cf6e02005ed10eb8635b4082fc0d43
divesh chauhan <diveshc207@gmail.com>
2025-03-29 10:58:02 +0530

    feat(product route) : Added show product details route

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const upload = require('../media/fileUpload');
-const { addProducts, showProducts } = require('../controllers/productController');
+const { addProducts, showProducts, showProductDetails } = require('../controllers/productController');
 
 router.get('/add', (req, res) => res.render('addProducts'));
 router.post('/add', upload.single('image'), addProducts);
 router.get('/show', showProducts);
+router.get('/details/:productId', showProductDetails);
 
 
 module.exports = router;
