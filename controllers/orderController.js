76ed0254774eec31b59a86b646288130063247b5
divesh chauhan <diveshc207@gmail.com>
2025-04-04 19:51:55 +0530

    fix(order controller): Correct query and ensure product removal

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -85,7 +85,33 @@ const removeOrder = async (req, res) => {
         order = await Order.findById(orderId).populate('products.productId');
 
         if(!order.products.legnth){
-           await Order.findOneAndUpdate({orderId})
+           await Order.findOneAndUpdate({orderId},
+            {
+               subtotal : 0,
+               tax : 0,
+               shipping : 0,
+               totalPrice : 0
+            }
+           )
+        } else {
+
+            let newSubtotal = order.products.reduce((acc, product)=> acc + product.productId.price * product.quantity);
+            let newTax = newTax * 0.1;
+            let newShipping = newShipping > 1000 ? 0 : 100;
+            let newTotalPrice = newSubtotal + newTax + newShipping;
+
+            await Order.findOneAndUpdate({ orderId }, 
+                {
+                    subtotal : newSubtotal,
+                    tax : newTax,
+                    shipping : newShipping,
+                    totalPrice : newTotalPrice
+                },
+                { new : true }
+            );
+
+            res.set('Cache-Controll','no-store');
+            res.redirect('/order/show');
         }
     } catch (error) {
         console.log(error);
@@ -95,5 +121,6 @@ const removeOrder = async (req, res) => {
 
 module.exports = {
     placeOrder,
-    showOrder
+    showOrder,
+    removeOrder
 }
