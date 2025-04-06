d63d46bedca975070d652d2408d9a6f5b053b1f8
divesh chauhan <diveshc207@gmail.com>
2025-04-06 11:32:58 +0530

    feat(order route): Added order route to app

--- app.js
+++ app.js
@@ -33,10 +33,12 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 const productRouter = require('./routes/productRoute');
 const userRouter = require('./routes/userRoute');
 const cartRouter = require('./routes/cartRoute');
+const orderRouter = require('./routes/orderRoute');
 
 app.use('/product', productRouter);
 app.use('/user/auth', userRouter);
 app.use('/cart', cartRouter);
+app.use('/order', orderRouter);
 app.use('/', (req, res) => res.render('index'));
 
 
