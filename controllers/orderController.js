30c045fd1d78798c2c0540559b3d4e2f652b73d7
divesh chauhan <diveshc207@gmail.com>
2025-04-06 20:00:23 +0530

    feat(order): Added remove order controller

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const Cart = require('../models/cartModel');
 const Order = require('../models/orderModel');
 
@@ -60,7 +61,61 @@ const showOrder = async (req, res) => {
 };
 
 
+// @des Remove Order
+// @route POST/order/remove
+
+const removeOrder = async (req, res) => {
+    try {
+        const { orderId, productId } = req.body;
+        const objectId = new mongoose.Types.ObjectId(productId);
+
+        let order = await Order.findByIdAndUpdate({orderId},
+            {$pull : { products : { productId : objectId }}},
+            { new : true }
+        );
+
+        if(!order){
+            return res.status(400).json({error:'order was not removed ❌'});
+        }
+
+        order = await order.findById(orderId).populate('products.productId');
+
+        if(!order.products.length){
+            await Order.findByIdAndUpdate({orderId},
+                {
+                    subtotal : 0,
+                    tax : 0,
+                    shipping : 0,
+                    totalPrice : 0
+                }
+            );
+        } else {
+            let newSubtotal = order.products.reduce((acc, product) => acc + product.productId.price * product.quantity,0);
+            let newTax = newSubtotal * 0.1;
+            let newShipping = newSubtotal > 1000 ? 0 : 100;
+            let newPrice = newSubtotal + newTax + newShipping;
+            
+            await Order.findByIdAndUpdate({ orderId }, 
+                {
+                    subtotal : newSubtotal,
+                    tax : newTax,
+                    shipping : newShipping,
+                    price : newPrice
+                }
+            );
+            
+            res.set('Cache-Control','no-store');
+            res.redirect('/order/show');
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error:'failed to remove order ❌'});
+    }
+};
+
+
 module.exports = {
     placeOrder,
-    showOrder
+    showOrder,
+    removeOrder
 }
