e6fa2962449d0d77f35058485d30dda01a1046f5
divesh chauhan <diveshc207@gmail.com>
2025-04-06 11:06:59 +0530

    feat(order model): Added order model

---
+++ models/orderModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+    userId:{
+        type:Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    products : [{
+      productId : {
+        type:Schema.Types.ObjectId,
+        ref:'Product',
+        required:true
+      },
+      quantity : {
+        type:Number,
+        required:true
+      },
+      subTotal : {
+        type:Number,
+        required:true
+      }
+    }],
+
+    subtotal : {
+        type:Number,
+        required:true
+    },
+    tax: {
+        type:Number,
+        required:true
+    },
+    shipping: {
+        type:Number,
+        required:true
+    },
+    totalPrice: {
+        type:Number,
+        required:true
+    },
+    status: {
+      type:String,
+      enum : ['Pending', 'Paid', 'Canceled'],
+      required:true
+    },
+},{timestamps:true});
+
+
+const Order = mongoose.model('Order', orderSchema);
+
+
+module.exports = Order;
