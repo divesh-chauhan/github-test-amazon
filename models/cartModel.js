bd3630c51c8ecd400f94cc729380661c86951dc9
divesh chauhan <diveshc207@gmail.com>
2025-04-06 10:09:39 +0530

    fix(cart model): Rename product quantity type string to number

--- models/cartModel.js
+++ models/cartModel.js
@@ -15,7 +15,7 @@ const cartSchema = new Schema({
             required:true
         },
         quantity:{
-            type:String,
+            type:Number,
             required:true
         }
     }]
