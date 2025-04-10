f89850c951c765502651c3258e93bbffb7352a5d
divesh chauhan <diveshc207@gmail.com>
2025-04-10 10:45:30 +0530

    fix(route): Fix payment route with changing their sequence

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const {  showPaymentPage, payment, showPaymentDetails } = require('../controllers/paymentController');
 
+router.get('/details/:orderId', showPaymentDetails);
 router.get('/:orderId', showPaymentPage);
 router.post('/pay', payment);
-router.get('/details/:orderId', showPaymentDetails);
 
 
 module.exports = router;
